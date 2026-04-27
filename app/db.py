4a6d581dd0aae107d4ba4c41928632bbb417dddd
Viktor <viktorstojanovski32@gmail.com>
2026-04-27 22:59:30 +0200

    Small bugfix

--- app/db.py
+++ app/db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
 engine = create_engine(
-    "postgresql://discover_user:discover_passord@localhost:5432/discover_local_activities"
+    "postgresql://discover_user:discover_password@localhost:5432/discover_local_activities"
 )
 
 SessionLocal = sessionmaker(bind=engine)
