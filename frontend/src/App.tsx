b8f187f3ae0f9b0aac92301f132e9c3b0555b4d7
Viktor <viktorstojanovski32@gmail.com>
2026-05-26 19:30:53 +0200

    Frontend React initialization

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='text-red-500 text-center '>Hello World</div>
+    </>
+  )
+}
+
+export default App
